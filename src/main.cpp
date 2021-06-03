ea20f74ac571a68bdee21c114fe98b40868caf97
Alexander Ramian <a.ramian@outlook.de>
2021-06-03 14:34:47 +0200

    fix: Ble service namespace

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,7 @@
 #include <vtproto.pb.h>
 #include <vtproto.h>
 #include "ble_pb_callback.h"
+// #include <devboard.h>
 #include "config.h"
 
 namespace tact
@@ -14,19 +15,17 @@ namespace tact
   {
     const uint16_t kBufferSize = 2048;
     uint8_t vt_message_buffer[kBufferSize];
-
   }
 }
 
 void setup()
 {
-  Serial.begin(9600);
+  Serial.begin(115200);
   while (!Serial)
   {
     /* code */
   }
-  using namespace tact::ble;
-  BLEDevice::init("device name");
+  BLEDevice::init("Devboard");
   BLEServer *server = BLEDevice::createServer();
   server->setCallbacks(new tact::ble::BleConnectionCallback());
 
@@ -36,21 +35,21 @@ void setup()
   pAdvertising->setMinPreferred(0x12);
 
   // Setup Service and its Characteristics
-  BLEService *service = server->createService(service_tactile_display::kServiceUuid, 1 + (service_tactile_display::kNumberOfCharacteristics * 5));
-  pAdvertising->addServiceUUID(service_tactile_display::kServiceUuid);
-  BLECharacteristic *characteristics[service_tactile_display::kNumberOfCharacteristics];
-  createCharacteristicsFromDescription(service, characteristics, service_tactile_display::characteristic_descriptions, service_tactile_display::kNumberOfCharacteristics);
+  BLEService *service = server->createService(tact::ble::service_tactile_display::kServiceUuid, 1 + (tact::ble::service_tactile_display::kNumberOfCharacteristics * 5));
+  pAdvertising->addServiceUUID(tact::ble::service_tactile_display::kServiceUuid);
+  BLECharacteristic *characteristics[tact::ble::service_tactile_display::kNumberOfCharacteristics];
+  createCharacteristicsFromDescription(service, characteristics, tact::ble::service_tactile_display::characteristic_descriptions, tact::ble::service_tactile_display::kNumberOfCharacteristics);
 
   // Assign Values to Characteristics
-  characteristics[service_tactile_display::kCharacteristicNumberOfOutputs]->setValue((uint8_t *)&tact::display::number_of_motors, 1);
-  characteristics[service_tactile_display::kCharacteristicDisplayPlaybackState]->setValue("IDLE");
-  // characteristics[service_tactile_display::kCharacteristicDisplayPlaybackRequestStop]->setValue((uint16_t)0); // No need to write?
+  characteristics[tact::ble::service_tactile_display::kCharacteristicNumberOfOutputs]->setValue((uint8_t *)&tact::display::number_of_motors, 1);
+  characteristics[tact::ble::service_tactile_display::kCharacteristicDisplayPlaybackState]->setValue("IDLE");
+  // characteristics[tact::ble::service_tactile_display::kCharacteristicDisplayPlaybackRequestStop]->setValue((uint16_t)0); // No need to write?
   //TODO Check if value of constant is equal to propagated value
-  characteristics[service_tactile_display::kCharacteristicModeVtprotoMaxMsgLength]->setValue((uint16_t &)tact::vtproto::kBufferSize);
+  characteristics[tact::ble::service_tactile_display::kCharacteristicModeVtprotoMaxMsgLength]->setValue((uint16_t &)tact::vtproto::kBufferSize);
 
   // Assign Callbacks to Characteristics
-  characteristics[service_tactile_display::kCharacteristicModeVtprotoBuffer]->setCallbacks(new tact::ble::VtprotoImidiateExecutionCallback());
-  // characteristics[service_tactile_display::kCharacteristicDisplayPlaybackRequestStop]->setCallbacks(new s);
+  characteristics[tact::ble::service_tactile_display::kCharacteristicModeVtprotoBuffer]->setCallbacks(new tact::ble::VtprotoImidiateExecutionCallback());
+  // characteristics[tact::ble::service_tactile_display::kCharacteristicDisplayPlaybackRequestStop]->setCallbacks(new s);
 
   service->start();
   BLEDevice::startAdvertising();
