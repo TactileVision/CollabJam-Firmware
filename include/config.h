721131ebc5d37d95220092c054ae004fd57396e8
Alexander Ramian <a.ramian@outlook.de>
2021-10-27 15:46:25 +0200

    config: Added three channel helmet config

--- include/config.h
+++ include/config.h
@@ -4,13 +4,16 @@
 #include "tactile_display.pb.h"
 
 // LIBRARY WIDE OVERRIDE VALUES
-#define TACTILE_DISPLAY_NUM_OUTPUTS 8
+#define TACTILE_DISPLAY_NUM_OUTPUTS 3
 
 namespace config {
 namespace display {
 
+// const uint16_t kNumOfOutputs = TACTILE_DISPLAY_NUM_OUTPUTS;
+// const uint8_t kMotorPins[kNumOfOutputs] = {13, 27, 26, 25, 33, 32, 19, 21};
+
 const uint16_t kNumOfOutputs = TACTILE_DISPLAY_NUM_OUTPUTS;
-const uint8_t kMotorPins[kNumOfOutputs] = {13, 27, 26, 25, 33, 32, 19, 21};
+const uint8_t kMotorPins[kNumOfOutputs] = {0, 1, 2};
 
 const uint8_t kAvailableOutputModesLen = 3;
 const uint8_t kAvailableMotorTypesLen = 3;
